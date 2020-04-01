68c4cdafa0edc2763fb54dc656ae611abcccf508
FiniteReality <digpoealternate@gmail.com>
2020-04-01 19:26:34 +0100

    Add scaffolding for new transcoders

--- src/Core/Transcoders/Pcm/PcmAudioTranscoderFactory.cs
+++ src/Core/Transcoders/Pcm/PcmAudioTranscoderFactory.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.IO.Pipelines;
 using Thermite.Codecs;
 using Thermite.Transcoders.Pcm;
@@ -15,14 +16,52 @@ namespace Thermite.Transcoders
         public IAudioTranscoder GetTranscoder(IAudioCodec codec,
             PipeReader input)
         {
-            if (codec is PcmAudioCodec pcmInfo)
-                return new PcmAudioTranscoder(input, pcmInfo);
+            if (!(codec is PcmAudioCodec pcmCodec))
+            {
+                ThrowArgumentException(nameof(codec),
+                    $"Invalid codec passed to " +
+                    $"{nameof(PcmAudioTranscoderFactory)}");
 
-            ThrowArgumentException(nameof(codec),
-                $"Invalid codec passed to " +
-                $"{nameof(PcmAudioTranscoderFactory)}");
+                return default;
+            }
 
-            return default;
+            return pcmCodec switch
+            {
+                { ChannelCount: 2, SamplingRate: 48000, BitDepth: 16,
+                  Format: SampleFormat.SignedInteger,
+                  Endianness: SampleEndianness.LittleEndian }
+                    => new PcmEncodingTranscoder(input, pcmCodec),
+
+                { Endianness: var endian } when
+                    endian != SampleEndianness.LittleEndian
+                    => new PcmEndiannessTranscoder(input, pcmCodec),
+
+                { Format: var format } when
+                    format != SampleFormat.SignedInteger
+                    => new PcmFormatTranscoder(input, pcmCodec),
+
+                { BitDepth: var bitDepth } when
+                    bitDepth != 16
+                    => new PcmBitDepthTranscoder(input, pcmCodec),
+
+                { SamplingRate: var samplingRate } when
+                    samplingRate != 48000
+                    => new PcmResamplingTranscoder(input, pcmCodec),
+
+                { ChannelCount: 1 }
+                    => new PcmStereoTranscoder(input, pcmCodec),
+                { ChannelCount : _ }
+                    => new PcmDownmixingTranscoder(input, pcmCodec),
+
+                _ => InvalidCodec()
+            };
+
+            static IAudioTranscoder InvalidCodec()
+            {
+                Debug.Fail("Invalid codec properties");
+
+                return null;
+            }
         }
 
         /// <inheritdoc/>
